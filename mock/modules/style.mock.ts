0ae622b2c4db5be79f2ace5f05488236f9fb576d
wyc <57949470+wuyichen1@users.noreply.github.com>
2025-12-26 14:54:55 +0800

    anniu

--- mock/modules/style.mock.ts
+++ mock/modules/style.mock.ts
@@ -102,7 +102,7 @@ export default {
   /** 高亮按钮样式 */
   highlightBtnStyle: {
     color: 'rgba(255, 255, 255, 1)',
-    backgroundColor: 'linear-gradient(151.23deg, rgba(71, 252, 255, 1) 0%, rgba(255, 94, 135, 1) 100%)'
+    backgroundColor: 'linear-gradient(105.44deg, rgba(28, 123, 255, 1) 0%, rgba(0, 0, 0, 1) 51.28%, rgba(255, 129, 51, 1) 100%)'
   },
   /** 确认按钮样式 */
   confirmBtnStyle: {
@@ -120,7 +120,8 @@ export default {
     /** 发送背景色 */
     sendBgColor: '#434044',
     /** 接收背景色 */
-    receiveBgColor: 'linear-gradient(157.71deg, rgba(255, 94, 135, 1) 0%, rgba(71, 252, 255, 1) 100%)'
+    receiveBgColor: 'linear-gradient(201.44deg, rgba(28, 123, 255, 1) 0%, rgba(0, 0, 0, 1) 51.28%, rgba(255, 129, 51, 1) 100%)'
+    // receiveBgColor: 'linear-gradient(157.71deg, rgba(255, 94, 135, 1) 0%, rgba(71, 252, 255, 1) 100%)'
   },
   /** 举报按钮 */
   reportBtnStyle: {
@@ -334,7 +335,7 @@ export default {
       height: '100px',
       borderRadius: '24px',
       backgroundColor: 'rgba(35, 30, 36, 1)',
-      selectedColor: 'linear-gradient(151.23deg, rgba(255, 94, 135, 1) 0%, rgba(48, 156, 133, 1) 100%)'
+      selectedColor: 'linear-gradient(105.44deg, rgba(28, 123, 255, 1) 0%, rgba(0, 0, 0, 1) 51.28%, rgba(255, 129, 51, 1) 100%)'
     }
   }
 } satisfies Window['styleJson']
