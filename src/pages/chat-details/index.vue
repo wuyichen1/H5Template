481886265acf6ea6b2c11678d37b535352af0a6b
wyc <57949470+wuyichen1@users.noreply.github.com>
2025-12-29 19:11:21 +0800

    img

--- src/pages/chat-details/index.vue
+++ src/pages/chat-details/index.vue
@@ -2,6 +2,7 @@
   import axios from 'axios'
   import CryptoJS from 'crypto-js'
   import { useUserStore } from '@/stores'
+  import chatIcon from '@/assets/public/ai_chat_icon.png'
 
   defineOptions({
     name: 'ChatDetails'
@@ -126,15 +127,25 @@
       <ul p-layout-padding>
         <li @click="onSend('I\'m feeling great today.')">
           <span>I'm feeling great today.</span>
-          <van-icon name="chat-o" />
+          <van-image
+            src="chatIcon"
+            :style="{ width: '24px', height: '24px' }"
+          />
         </li>
         <li @click="onSend('Do you like reading?')">
           <span>Do you like reading?</span>
-          <van-icon name="chat-o" />
+          <van-image
+            src="chatIcon"
+            :style="{ width: '24px', height: '24px' }"
+          />
         </li>
         <li @click="onSend('Can you comfort me?')">
           <span>Can you comfort me?</span>
-          <van-icon name="chat-o" />
+          <!-- <van-icon name="chat-o" /> -->
+          <van-image
+            src="chatIcon"
+            :style="{ width: '24px', height: '24px' }"
+          />
         </li>
       </ul>
     </div>
