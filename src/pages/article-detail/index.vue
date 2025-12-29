bc04b620367d9aadfdb214dd7a400e7ef0a14e6a
wyc <57949470+wuyichen1@users.noreply.github.com>
2025-12-29 15:39:30 +0800

    09

--- src/pages/article-detail/index.vue
+++ src/pages/article-detail/index.vue
@@ -188,7 +188,7 @@
 
   .comment-content {
     padding: 16px;
-    background:  #282329;
+    background:  #282329d1;
     border-radius: var(--ai-rounded, 8px);
     display: flex;
     flex-direction: column;
