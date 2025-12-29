29e4800b2b17aa3305770175dc9815f340922349
wyc <57949470+wuyichen1@users.noreply.github.com>
2025-12-29 14:50:23 +0800

    ¥

--- src/pages/my-modules/gold-coin.vue
+++ src/pages/my-modules/gold-coin.vue
@@ -1,25 +1,25 @@
 <script setup lang="ts">
-  import MasonryIcon from '@/assets/public/masonry-icon.png'
-  import MyIcon from '@/assets/public/my-icon.png'
-  import { useJump } from '@/hooks/useJump'
-  import { useWindow } from '@/hooks/useWindow'
-  import { useUserStore } from '@/stores'
+import MasonryIcon from "@/assets/public/masonry-icon.png";
+import MyIcon from "@/assets/public/my-icon.png";
+import { useJump } from "@/hooks/useJump";
+import { useWindow } from "@/hooks/useWindow";
+import { useUserStore } from "@/stores";
 
-  defineOptions({
-    name: 'GoldCoin'
-  })
+defineOptions({
+  name: "GoldCoin",
+});
 
-  const { userInfo } = useUserStore()
-  const { winCoinData } = useWindow()
-  const { appParams } = useJump()
+const { userInfo } = useUserStore();
+const { winCoinData } = useWindow();
+const { appParams } = useJump();
 
-  const formData = reactive({
-    radio: winCoinData[0]?.key
-  })
+const formData = reactive({
+  radio: winCoinData[0]?.key,
+});
 
-  const onRecharge = () => {
-    appParams({ key: 'Recharge', value: formData.radio, state: 1 })
-  }
+const onRecharge = () => {
+  appParams({ key: "Recharge", value: formData.radio, state: 1 });
+};
 </script>
 
 <template>
@@ -45,11 +45,11 @@
           :class="{ 'on-active': formData.radio === item.key }"
           @click="formData.radio = item.key"
         >
+          <span ai-text-desc>{{ item.meney }}$</span>
           <p>
             <van-image h-4 w-5 :src="MyIcon" fit="cover" />
             <span ml-1 ai-user-name>{{ item.cions }}</span>
           </p>
-          <span ai-text-desc>{{ item.meney }}$</span>
         </li>
       </ul>
 
@@ -62,53 +62,51 @@
 </template>
 
 <style lang="less" scoped>
-  .gold-coin_box {
-    padding-top: calc(
-      var(--van-nav-bar-height) + var(--ai-view-padding-top)
-    );
-    min-height: 100vh;
-    background: var(--ai-coin-bg-color);
-  }
+.gold-coin_box {
+  padding-top: calc(var(--van-nav-bar-height) + var(--ai-view-padding-top));
+  min-height: 100vh;
+  background: var(--ai-coin-bg-color);
+}
 
-  .top-box {
-    border-radius: 20px;
-    background: url(@/assets/public/diamonds_bg.png);
-    background-size: 100% 100%;
-    background-repeat: no-repeat;
-    // border: 4px solid rgba(255, 255, 255, 0.09);
+.top-box {
+  border-radius: 20px;
+  background: url(@/assets/public/diamonds_bg.png);
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  // border: 4px solid rgba(255, 255, 255, 0.09);
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  padding-bottom: 10px;
+}
+
+.bottom-selsect {
+  margin-top: 16px;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  /* 每行 3 列，每列等宽 */
+  gap: 10px;
+  /* 可选：设置子项之间的间距 */
+
+  li {
+    width: var(--ai-coin-select-style-width);
+    height: var(--ai-coin-select-style-height);
     display: flex;
+    flex-direction: column;
     align-items: center;
-    padding: 20px;
-    padding-bottom: 10px;
-  }
+    justify-content: center;
+    border-radius: var(--ai-coin-select-style-border-radius);
+    background: var(--ai-coin-select-style-bg-color);
 
-  .bottom-selsect {
-    margin-top: 16px;
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    /* 每行 3 列，每列等宽 */
-    gap: 10px;
-    /* 可选：设置子项之间的间距 */
-
-    li {
-      width: var(--ai-coin-select-style-width);
-      height: var(--ai-coin-select-style-height);
+    p {
+      margin-bottom: 8px;
       display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      border-radius: var(--ai-coin-select-style-border-radius);
-      background: var(--ai-coin-select-style-bg-color);
-
-      p {
-        margin-bottom: 8px;
-        display: flex;
-        align-items: baseline;
-      }
-    }
-
-    .on-active {
-      background: var(--ai-coin-select-style-selected-color);
+      align-items: baseline;
     }
   }
+
+  .on-active {
+    background: var(--ai-coin-select-style-selected-color);
+  }
+}
 </style>
