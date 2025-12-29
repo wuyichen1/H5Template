60157de923614ed7827aa91a797838ce12a07a42
wyc <57949470+wuyichen1@users.noreply.github.com>
2025-12-29 14:34:16 +0800

    rad

--- src/components/TextBox.vue
+++ src/components/TextBox.vue
@@ -25,7 +25,7 @@ const props = withDefaults(defineProps<{
       :maxlength="props.maxlength"
       placeholder="Please enter"
       show-word-limit
-      :style="{ '--text-box-bg': props.bg }"
+      :style="{ '--text-box-bg': props.bg, borderRadius: '12px' }"
     />
   </div>
 </template>
