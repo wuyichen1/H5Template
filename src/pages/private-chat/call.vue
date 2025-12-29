917a53cd9ef0bb623a73a3f03e61675c3dafd684
wyc <57949470+wuyichen1@users.noreply.github.com>
2025-12-29 15:11:24 +0800

    bg5

--- src/pages/private-chat/call.vue
+++ src/pages/private-chat/call.vue
@@ -75,5 +75,22 @@
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
+    position: relative;
+
+    &::before {
+      content: '';
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.5);
+      z-index: 1;
+    }
+
+    > * {
+      position: relative;
+      z-index: 2;
+    }
   }
 </style>
