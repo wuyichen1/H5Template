115f634f07c922903850ef3feee621719303db83
wyc <57949470+wuyichen1@users.noreply.github.com>
2025-12-29 11:42:11 +0800

    AG

--- src/pages/my-modules/setup-page.vue
+++ src/pages/my-modules/setup-page.vue
@@ -15,6 +15,12 @@
     { label: 'Blacklist', value: '2' }
   ]
 
+  const userAndPrivacyAgreement = [
+    'We hereby advise you to carefully read and understand this User Agreement (hereinafter referred to as “Agreement”)before registering. In order to clarify rights and obligations of all parties,and protect the legitimate rights and interests of all parties,this Agreement is specially formulated.the contents highlighted in bold underline in this Agreement are specially reminded of your attention.Please ensure that you fully understand this Agreement.You are not authorized to register ,log in or use our services or software(hereinafter referred to as services, software)unless you accept all the Agreement.Your registration,login,usage,etc.will bu deemed as acceptance of this Agreement and you agree to be bound by the Agreement. We reserve the right,at our discretion, to change,modify,add,or remove portions of the Agreement,sup;ementary Agreement,Privacy Policy,regulative provisions or feature terms at any time by posting the amended version on our software or within the services without prior noice.If you do not agree to these terms or any future amended version,then you must not access,use our services.Otherwise,your access,use of our services shall be deemed to agree any future updated version of them.',
+    'We hereby advise you to carefully read and understand this User Agreement (hereinafter referred to as “Agreement”)before registering. In order to clarify rights and obligations of all parties,and protect the legitimate rights and interests of all parties,this Agreement is specially formulated.the contents highlighted in bold underline in this Agreement are specially reminded of your attention.Please ensure that you fully understand this Agreement.You are not authorized to register ,log in or use our services or software(hereinafter referred to as services, software)unless you accept all the Agreement.Your registration,login,usage,etc.will bu deemed as acceptance of this Agreement and you agree to be bound by the Agreement. We reserve the right,at our discretion, to change,modify,add,or remove portions of the Agreement,sup;ementary Agreement,Privacy Policy,regulative provisions or feature terms at any time by posting the amended version on our software or within the services without prior noice.If you do not agree to these terms or any future amended version,then you must not access,use our services.Otherwise,your access,use of our services shall be deemed to agree any future updated version of them.',
+    'Blacklist'
+  ]
+
   const onState = (key: AppCommunication) => {
     appParams({ key, state: 2 })
   }
@@ -40,7 +46,7 @@
         :key="item.value"
         @click="onSelect(item.value)"
       >
-        <span>{{ item.label }}</span>
+        <span>{{ userAndPrivacyAgreement[Number(item.value)] }}</span>
         <van-image h-3 w-4 :src="RightIcon" fit="cover" />
       </li>
     </ul>
