28ae3e112d5f846e0fb4088829478e84c08e966f
wyc <57949470+wuyichen1@users.noreply.github.com>
2026-01-12 17:56:45 +0800

    bg

--- src/App.vue
+++ src/App.vue
@@ -70,13 +70,15 @@ onMounted(() => {
   left: 0;
   width: 100%;
   height: 100%;
-  background: linear-gradient(180deg, rgba(32, 25, 43, 0) 0%, rgba(32, 25, 43, 1) 100%),
-    linear-gradient(90deg, rgba(57, 44, 176, 1) 0%, rgba(110, 12, 149, 1) 100%);
+  background-image: url('@/assets/public/bg.png');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
   pointer-events: none;
   z-index: 1;
 }
 
-/* 确保所有页面内容在蒙版之上 */
+/* 确保所有页面内容在背景之上 */
 .app-wrapper > * {
   position: relative;
   z-index: 2;
