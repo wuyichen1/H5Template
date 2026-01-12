7f0e4bd53d35598015826a0ca7e1f3b7e0993da8
wyc <57949470+wuyichen1@users.noreply.github.com>
2026-01-12 18:27:08 +0800

    juz

--- src/components/Empty.vue
+++ src/components/Empty.vue
@@ -4,23 +4,32 @@
 
 <template>
   <div class="empty-container">
-    <van-image
-      :src="EmptyIcon"
-      fit="scale-down"
-      class="empty-image"
-    />
+    <div class="empty-image-wrapper">
+      <van-image
+        :src="EmptyIcon"
+        fit="scale-down"
+        class="empty-image"
+      />
+    </div>
   </div>
 </template>
 
 <style lang="less" scoped>
 .empty-container {
+  width: 100%;
+  height: 120px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: transparent;
+}
+
+.empty-image-wrapper {
   width: 120px;
   height: 120px;
   display: flex;
   justify-content: center;
   align-items: center;
-  /* 让图片完整显示不被裁切 */
-  background: transparent;
 }
 
 .empty-image {
@@ -29,5 +38,6 @@
   object-fit: scale-down;
   border-radius: 50%;
   background: transparent;
+  display: block;
 }
 </style>
