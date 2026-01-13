9e1555b4c999bc7788d0ecf3e03b72626a6b6b79
wyc <57949470+wuyichen1@users.noreply.github.com>
2026-01-13 16:27:56 +0800

    tk

--- src/components/PopupBox.vue
+++ src/components/PopupBox.vue
@@ -15,9 +15,4 @@ const show = defineModel<boolean>('show', {
 </template>
 
 <style lang="less" scoped>
-  .popup-box {
-    :deep(.van-popup) {
-      padding-top: 0 !important;
-    }
-  }
 </style>
