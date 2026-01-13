0df7e597e7bc6879b3b0b3d02d2fbe41b2acea04
wyc <57949470+wuyichen1@users.noreply.github.com>
2026-01-13 14:58:11 +0800

    no

--- src/components/Empty.vue
+++ src/components/Empty.vue
@@ -20,8 +20,8 @@
 <style lang="less" scoped>
 .empty-container {
   width: 100%;
-  height: 120px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   background: transparent;
