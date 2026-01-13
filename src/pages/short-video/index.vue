cc12b0120cbf1fba8d37b9f9791904c49948e2ef
wyc <57949470+wuyichen1@users.noreply.github.com>
2026-01-13 17:19:24 +0800

    pd

--- src/pages/short-video/index.vue
+++ src/pages/short-video/index.vue
@@ -155,7 +155,7 @@ const togglePlay = async () => {
       v-model:show="isPopup"
       background="linear-gradient(90deg, rgba(46, 35, 146, 1) 0%, rgba(83, 9, 112, 1) 100%)"
     >
-      <div p-layout-padding>
+      <div style="padding-top:16px; padding-bottom:16px;">
         <van-divider content-position="left">Comments</van-divider>
       </div>
       <div class="h-[56vh] overflow-y-auto">
